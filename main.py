678970c2ecf7de29b9cc3145a3eec2ac7e2e0298
Weiying Zhao <winzhao_0545@yahoo.com>
2023-06-16 12:00:58 +0200

    game

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-main
+from utils.game import Hangman
+#create a new game for Hangman.
+new_game=Hangman()
+new_game.play()
--- utils/game.py
+++ utils/game.py
@@ -1,19 +1,94 @@
+import random
+
 class Hangman:
-    #Attributes of the class
-     #Words to be selected
-    def __init__(self) -> None:
-        self.possible_words=['becode','learning','mathematics','sessions']
-        self.word_to_find=list(random.choice(self.possible_words))
-     #players'lives in the game
-        self.lives=5
-     #letters that are correctly guessed 
-        self.correctly_guessed_letters=[]
-     #letters that are wrongly guessed
-        self.wrongly_guessed_letters=[]
-     #numbers played by players
-        self.turn_count=0  
-     #errors played by players.           
-        self.error_count =0           
-    #create a method to play the game.
-    def play(self):
+  """
+  Function that initiate the class.
+  """
+  def __init__(self):
+    self.possible_words=['becode','learning','mathematics','sessions']
+    self.word_to_find=random.choice(self.possible_words)
+    self.lives=5
+    self.correctly_guess_letter=["_"]*len(self.word_to_find)
+    self.wrongly_guess_letter=[]
+    self.turn_count=0
+    self.error_count=0
+  
     
+  def play(self):
+    """
+    Function that ask people to enter letters:
+    -Enter ONE letter
+    -If right goes to correctly_guess_letter
+    -If wrong, letter goes to wrongly_guessed_letters
+    """
+    
+    Input=''
+    ask=True
+    while ask:
+      Input=input("Enter a single letter:")
+
+
+
+
+      #Enter ONE letter
+      print(len(Input))
+      
+      if len(Input)==1:
+        ask=False
+        print(Input.lower())
+       
+      
+      
+        
+      else:
+        print('Enter a single letter to continue')
+        
+
+
+    #  correct word goes into    
+    for i in self.word_to_find:
+    
+      if Input==i:
+        self.correctly_guess_letter.append(i)
+        print(self.correctly_guess_letter)
+        self.turn_count +=1
+        
+
+      else:
+        self.wrongly_guess_letter.append(i)
+        print(self.wrongly_guess_letter)
+        self.error_count +=1
+        self.lives-=1
+      if Input==self.word_to_find:
+        self.well_play()
+
+
+  def start_game(self):
+    """
+    Function that start to play the loop of game.
+    """
+    print("Welcome to Hangman.")
+    self.play()
+    
+    print(self.correctly_guess_letter) 
+    print(self.wrongly_guess_letter)
+    print(self.lives)
+    print(self.error_count) 
+    print(self.turn_count)
+    #user guess the word→ input = word_to_find 
+   
+    print('Congratuations, you win the game!')
+    #Game over→livers=0
+    
+      
+  def game_over(self):
+      if self.lives==0:   
+        print('game over.')
+    
+  def well_play(self):
+    print(f'You find the word: ,{self.word_to_find},in,{self.turn_count},turns with,{self.liveserror_count},errors')
+
+    
+
+
+  
